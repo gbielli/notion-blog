40681f43a40d4b97e473b13eab2aa300ca7e4285
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-06-18 18:33:27 +0200

    noimg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
   <div className="max-w-xl mx-auto">
     <div className="flex flex-col gap-3 mx-auto text-center">
-      <img src="https://static.vecteezy.com/system/resources/previews/009/339/329/original/chicken-egg-raw-free-png.png" className="mx-auto" width={300} height={400} alt="" />
+      <img src="" className="mx-auto" width={300} height={400} alt="" />
       <h1 className="text-5xl font-bold">il était une fois, <span className="text-indigo-600">un chauve</span> qui écrivait pour les chauves.</h1>
       <h2>Se raser le crâne, c'est tout un art. Je suis rasé depuis plus de 5 ans et je te partage mes meilleures astuces pour que ton crâne brille de mille feux.</h2>
       <div className="inline-block">
