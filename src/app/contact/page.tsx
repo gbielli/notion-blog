eea351f748f5e6314edd0b81b54b91438fe297c4
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-06-18 18:38:44 +0200

    corr

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function ContactPage() {
   return (
     <>
     <h1>Le chauve qui écrit pour les chauves.</h1>
-    <h2>Se raser le crâne, c'est tout un art. Je suis rasé depuis plus de 5 ans et je vous partage mes meilleures astuces pour que votre crâne brille de mille feu.</h2>
+    <h2>Se raser le crâne, c&apos;est tout un art. Je suis rasé depuis plus de 5 ans et je vous partage mes meilleures astuces pour que votre crâne brille de mille feu.</h2>
     </>
 )
 }
