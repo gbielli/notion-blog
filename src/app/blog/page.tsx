fa9234d67bcd41bfe36ddaca3eceb6053bdab306
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-06-18 18:41:02 +0200

    noimages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -23,9 +23,8 @@ export default async function BlogPage() {
     <>
     <div className="max-w-xl mx-auto">
     <div className="flex flex-col gap-3 mx-auto text-center">
-      <img src="https://static.vecteezy.com/system/resources/previews/009/339/329/original/chicken-egg-raw-free-png.png" className="mx-auto" width={300} height={400} alt="" />
       <h1 className="text-5xl font-bold">il était une fois, <span className="text-indigo-600">un chauve</span> qui écrivait pour les chauves.</h1>
-      <h2>Se raser le crâne, c'est tout un art. Je suis rasé depuis plus de 5 ans et je te partage mes meilleures astuces pour que ton crâne brille de mille feux.</h2>
+      <h2>Se raser le crâne, cest tout un art. Je suis rasé depuis plus de 5 ans et je te partage mes meilleures astuces pour que ton crâne brille de mille feux.</h2>
       <div className="inline-block">
 
       <button className="px-5 py-2 py-3 bg-indigo-600 rounded-lg text-white">Mes derniers articles</button>
