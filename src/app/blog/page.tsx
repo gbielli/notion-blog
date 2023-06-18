58992e4b8341f99f2ef4da05852359c4b1819f9b
Guillaume BIELLI <Guillaumebielli@MacBook-Guitou.local>
2023-06-18 18:28:16 +0200

    new

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -21,6 +21,17 @@ export default async function BlogPage() {
 
   return (
     <>
+    <div className="max-w-xl mx-auto">
+    <div className="flex flex-col gap-3 mx-auto text-center">
+      <img src="https://static.vecteezy.com/system/resources/previews/009/339/329/original/chicken-egg-raw-free-png.png" className="mx-auto" width={300} height={400} alt="" />
+      <h1 className="text-5xl font-bold">il était une fois, <span className="text-indigo-600">un chauve</span> qui écrivait pour les chauves.</h1>
+      <h2>Se raser le crâne, c'est tout un art. Je suis rasé depuis plus de 5 ans et je te partage mes meilleures astuces pour que ton crâne brille de mille feux.</h2>
+      <div className="inline-block">
+
+      <button className="px-5 py-2 py-3 bg-indigo-600 rounded-lg text-white">Mes derniers articles</button>
+      </div>
+    </div>
+  </div>
       <section className="mb-16 mt-0 space-y-8 md:mt-20">
         <SearchBar />
         <CategoryFilter allCategories={allCategories} />
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,15 @@
+import Script from "next/script";
+
 export const metadata = {
   title: 'Contact',
   description: 'This is contact page.',
 };
 
 export default function ContactPage() {
-  return <h1 className="mt-12 text-center text-3xl font-bold">Contact</h1>;
+  return (
+    <>
+    <h1>Le chauve qui écrit pour les chauves.</h1>
+    <h2>Se raser le crâne, c'est tout un art. Je suis rasé depuis plus de 5 ans et je vous partage mes meilleures astuces pour que votre crâne brille de mille feu.</h2>
+    </>
+)
 }
